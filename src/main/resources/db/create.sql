a9b9ef86f51b46c64182b7f1c74f17be3d47fbf8
Serah wakaba <serahwakaba9@gmail.com>
2021-06-20 11:48:01 +0300

    description table created

--- src/main/resources/db/create.sql
+++ src/main/resources/db/create.sql
@@ -1,2 +1,8 @@
 CREATE DATABASE organisational_api;
 \c organisational_api;
+CREATE TABLE IF NOT EXISTS departments(
+ id serial PRIMARY KEY;
+ name VARCHAR;
+ description VARCHAR;
+ numberofemployees VARCHAR;
+ );
