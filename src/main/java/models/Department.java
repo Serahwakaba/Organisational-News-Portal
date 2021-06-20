8bcc60511560d1681ba01a9d08131c0e69eb7d54
Serah wakaba <serahwakaba9@gmail.com>
2021-06-20 12:07:56 +0300

    department getters and setters created

--- src/main/java/models/Department.java
+++ src/main/java/models/Department.java
@@ -1,10 +1,46 @@
 package models;
 
 public class Department {
-
     private int id;
     private String name;
     private String description;
+
+
+
     private int numberofemployees;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getNumberofemployees() {
+        return numberofemployees;
+    }
+
+    public void setNumberofemployees(int numberofemployees) {
+        this.numberofemployees = numberofemployees;
+    }
+
+
 
 }
