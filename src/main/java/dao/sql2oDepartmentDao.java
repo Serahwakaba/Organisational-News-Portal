80b792f7388dbaffac56d06b531cc9c0960ed102
Serah wakaba <serahwakaba9@gmail.com>
2021-06-20 14:57:34 +0300

    deleteid departments commit

--- src/main/java/dao/sql2oDepartmentDao.java
+++ src/main/java/dao/sql2oDepartmentDao.java
@@ -65,5 +65,17 @@ public class sql2oDepartmentDao {
                     .executeAndFetchFirst(Department.class);
         }
     }
+    @Override
+    public void deleteById(int id) {
+        String sql = "DELETE from departments WHERE id = :id";
+        try (Connection con = sql2o.open()) {
+            con.createQuery(sql)
+                    .addParameter("id", id)
+                    .executeUpdate();
+
+        } catch (Sql2oException ex){
+            System.out.println(ex);
+        }
+    }
 
 }
