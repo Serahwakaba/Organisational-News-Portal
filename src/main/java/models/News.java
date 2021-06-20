9d3f6dd3699a6065ed6b513828cf162b8f85e51c
Serah wakaba <serahwakaba9@gmail.com>
2021-06-20 13:19:52 +0300

    news getters and constructor created

--- src/main/java/models/News.java
+++ src/main/java/models/News.java
@@ -5,4 +5,40 @@ public class News {
     private int departmentid;
     private String content;
     private String description;
+
+
+    public News(String content, String description, int departmentid) {
+        this.content = content;
+        this.description = description;
+        this.departmentid = departmentid;
+    }
+    public String getContent() {
+        return content;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public int getDepartmentid() {
+        return departmentid;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setDepartmentid(int departmentid) {
+        this.departmentid = departmentid;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
