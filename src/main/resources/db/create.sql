b5021a3c647de06f1245fd23f3eaf49e5a9e634c
Serah wakaba <serahwakaba9@gmail.com>
2021-06-20 11:52:05 +0300

    news table created

--- src/main/resources/db/create.sql
+++ src/main/resources/db/create.sql
@@ -1,8 +1,16 @@
 CREATE DATABASE organisational_api;
 \c organisational_api;
+
 CREATE TABLE IF NOT EXISTS departments(
  id serial PRIMARY KEY;
  name VARCHAR;
  description VARCHAR;
  numberofemployees VARCHAR;
+ );
+
+ CREATE TABLE IF NOT EXISTS news (
+  id serial PRIMARY KEY ,
+  content VARCHAR,
+  description VARCHAR,
+  departmentid int
  );
