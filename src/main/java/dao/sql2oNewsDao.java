b3e5f52c1e56a4a79dc40bc7f56de8a9d8f2fd24
Serah wakaba <serahwakaba9@gmail.com>
2021-06-20 16:10:47 +0300

    insert values into news commit

--- src/main/java/dao/sql2oNewsDao.java
+++ src/main/java/dao/sql2oNewsDao.java
@@ -3,6 +3,7 @@ package dao;
 import models.News;
 import org.sql2o.Connection;
 import org.sql2o.Sql2o;
+import org.sql2o.Sql2oException;
 
 import java.util.List;
 
@@ -24,6 +25,19 @@ public class sql2oNewsDao implements NewsDao {
             return con.createQuery("SELECT * FROM news WHERE departmentid = :departmentid")
                     .addParameter("departmentid", departmentId)
                     .executeAndFetch(News.class);
+        }
+    }
+    @Override
+    public void add(News news) {
+        String sql = "INSERT INTO news (content, description, departmentid) VALUES (:content,:description, :departmentid)";
+        try(Connection con = sql2o.open()){
+            int id = (int) con.createQuery(sql, true)
+                    .bind(news)
+                    .executeUpdate()
+                    .getKey();
+            news.setId(id);
+        } catch (Sql2oException ex) {
+            System.out.println(ex);
         }
     }
 
